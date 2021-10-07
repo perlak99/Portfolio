aa6a0eca5bf6542a7320d8d617f0ed25ee01142e
Perlak99 <perlakslawomir@gmail.com>
2021-10-07 19:07:13 +0200

    photo update

--- script.js
+++ script.js
@@ -1,34 +1,17 @@
-const navButton = document.querySelector('.nav-button');
-const navMenu = document.querySelector('.nav-menu');
+const navButton = document.querySelector(".nav-button");
+const navMenu = document.querySelector(".nav-menu");
 
-const aboutMeNav = document.querySelector('.nav-about-me')
-const skillsNav = document.querySelector('.nav-skills');
-const projectsNav = document.querySelector('.nav-projects');
-const contactNav = document.querySelector('.nav-contact');
-
-const aboutMeDiv = document.querySelector('.about-me');
-const skillsDiv = document.querySelector('.skills');
-const projectsDiv = document.querySelector('.projects');
-const contactDiv = document.querySelector('.contact');
-
-navButton.addEventListener('click', () => {
-    navButton.classList.toggle("open");
-    navMenu.classList.toggle("open");
+navButton.addEventListener("click", () => {
+  navButton.classList.toggle("open");
+  navMenu.classList.toggle("open");
 });
 
-aboutMeNav.addEventListener('click', () => {
-   aboutMeDiv.scrollIntoView({behavior: "smooth", block: "center"});
+navMenu.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target.classList.contains("nav-menu-link")) {
+    const id = e.target.getAttribute("href");
+    const element = document.querySelector(id);
+    const y = element.getBoundingClientRect().top + window.pageYOffset - 90;
+    window.scrollTo({ top: y, behavior: "smooth" });
+  }
 });
-
-skillsNav.addEventListener('click', () => {
-    skillsDiv.scrollIntoView({behavior: "smooth", block: "center"});
-});
-
-projectsNav.addEventListener('click', () => {
-    projectsDiv.scrollIntoView({behavior: "smooth", block: "center"});
-});
-
-contactNav.addEventListener('click', () => {
-    contactDiv.scrollIntoView({behavior: "smooth", block: "center"});
-});
-
